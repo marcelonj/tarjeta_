a22c33ea5afb8dc79b7d291d56be915acfed06c9
Marcelo Jurado <marcelonjurado@gmail.com>
2022-11-07 00:21:01 -0300

    Correcciones de tamaño

--- script.js
+++ script.js
@@ -108,3 +108,30 @@ if((screen.width / screen.height) > 0.75){
     alert("Este sitio fue pensado para verse en dispositivos móviles, es posible que en su pantalla no se vea como originalmente fue pensado")
 }
 setTimeout(eliminar_mensaje, 5000);
+
+simplyCountdown('#contador', {
+    year: 2022, // required
+    month: 11, // required
+    day: 26, // required
+    hours: 21, // Default is 0 [0-23] integer
+    minutes: 30, // Default is 0 [0-59] integer
+    seconds: 0, // Default is 0 [0-59] integer
+    words: { //words displayed into the countdown
+        days: { singular: 'DÍAS', plural: 'Días' },
+        hours: { singular: 'HS', plural: 'Horas' },
+        minutes: { singular: 'MIN', plural: 'Minutos' },
+        seconds: { singular: 'SEG', plural: 'Segundos' }
+    },
+    plural: false, //use plurals
+    inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+    inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+    // in case of inline set to false
+    enableUtc: false, //Use UTC or not - default : false
+    onEnd: function() { return; }, //Callback on countdown end, put your own function here
+    refresh: 1000, // default refresh every 1s
+    sectionClass: 'simply-section', //section css class
+    amountClass: 'simply-amount', // amount css class
+    wordClass: 'simply-word', // word css class
+    zeroPad: false,
+    countUp: false
+});
